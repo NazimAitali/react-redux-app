ac2870b61e9749cad11eab60d4db515cc3e7f5cd
nazim <nazimaitali88@gmail.com>
2020-10-18 23:35:43 +0100

    Updates

---
+++ precache-manifest.42f32a4df5945cb997247010a182b1d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "461cadcda3d1c73be0377a7953427dff",
+    "url": "/react-redux-app/index.html"
+  },
+  {
+    "revision": "54ced8cdd6796d1d535e",
+    "url": "/react-redux-app/static/css/main.1a7ab21c.chunk.css"
+  },
+  {
+    "revision": "047664bfa98e8a5cce0c",
+    "url": "/react-redux-app/static/js/2.84ceede2.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/react-redux-app/static/js/2.84ceede2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ced8cdd6796d1d535e",
+    "url": "/react-redux-app/static/js/main.c113ea21.chunk.js"
+  },
+  {
+    "revision": "8ef7322aa62dc08bee15",
+    "url": "/react-redux-app/static/js/runtime-main.cd765655.js"
+  }
+]);
